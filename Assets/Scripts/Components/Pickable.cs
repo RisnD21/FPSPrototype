64b0e199f59566d2270b87fef14d8ffd3536f530
RisnD21 <risen1221hyw@gmail.com>
2025-09-06 23:38:55 +0800

    Minor Adjust
    Remove a blank line

--- Assets/Scripts/Components/Pickable.cs
+++ Assets/Scripts/Components/Pickable.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class Pickable : MonoBehaviour 
 {
-
     [HideInInspector] public ItemStack stack;
     [HideInInspector] public ItemManager Manager;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            if(ItemManager.Instance.TryPickItem(stack, transform.position))
+            if (ItemManager.Instance.TryPickItem(stack, transform.position))
             {
                 Debug.Log($"You've collected {stack}");
                 gameObject.SetActive(false);
-            }   
+            }
         }
     }
 }
